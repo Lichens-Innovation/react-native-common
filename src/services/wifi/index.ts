c65f916e02d8d144114d0b639736917b8cfa5141
André Masson <amwebexpert@gmail.com>
2025-10-07 11:12:06 -0400

    feat: wifi info context and appStateStatus MobX store attribute (b)

--- src/services/wifi/index.ts
+++ src/services/wifi/index.ts
@@ -3,3 +3,4 @@ export * from './use-wifi-info';
 export * from './wifi-info.types';
 export * from './wifi-info.utils.simulator';
 export * from './wifi-info.utils';
+export * from './wifi-infos-context';
