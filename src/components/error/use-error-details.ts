00797d85e9059954c896e8a45b4e67ac2da1e2cb
André Masson <massona@lichens.ai>
2025-11-29 09:14:11 -0400

    fix: error boundary stack trace to sentry (#11)

--- src/components/error/use-error-details.ts
+++ src/components/error/use-error-details.ts
@@ -9,11 +9,13 @@ export const useErrorDetails = (error: unknown) => {
   const title = t('common:error');
   const errorMsg = getErrorMessage(error) || 'Unknown';
   const description = t('common:errorBoundary.error', { id, errorMsg });
+  const stack = error instanceof Error ? error.stack : '';
 
   return {
     id,
     title,
     errorMsg,
     description,
+    stack,
   };
 };
