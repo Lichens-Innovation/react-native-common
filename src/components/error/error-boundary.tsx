e6e029cace6b4df195365ef5abe773bd9aa8cc0e
André Masson <amwebexpert@gmail.com>
2025-11-29 07:11:34 -0600

    fix: error boundary stack trace to sentry

--- src/components/error/error-boundary.tsx
+++ src/components/error/error-boundary.tsx
@@ -1,14 +1,24 @@
 import { ErrorBoundaryProps, useRouter } from 'expo-router';
+import * as Sentry from '@sentry/react-native';
 import { FunctionComponent, useEffect } from 'react';
 import { logger } from '../../logger/logger';
 import { useErrorDetails } from './use-error-details';
 
 export const ErrorBoundary: FunctionComponent<ErrorBoundaryProps> = ({ error }) => {
   const router = useRouter();
-  const { title, description, errorMsg, id } = useErrorDetails(error);
+  const { title, description, errorMsg, id, stack } = useErrorDetails(error);
 
   useEffect(() => {
     logger.error(`ErrorBoundary Unexpected error: ${errorMsg} (ID: ${id})`, error);
+    Sentry.captureException(error, {
+      extra: {
+        title,
+        description,
+        errorMsg,
+        id,
+        stack,
+      },
+    });
 
     router.replace({
       pathname: '/error',
