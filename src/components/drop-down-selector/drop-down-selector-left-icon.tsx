cf9d6ba566f69938f650a1d04e4fe29f96e8ed32
André Masson <amwebexpert@gmail.com>
2026-01-20 15:57:28 -0600

    feat: dropdown selector icon

--- src/components/drop-down-selector/drop-down-selector-left-icon.tsx
+++ src/components/drop-down-selector/drop-down-selector-left-icon.tsx
@@ -4,17 +4,19 @@ import { Icon } from 'react-native-paper';
 export type DropDownSelectorLeftIconProps = {
   icon?: string;
   color: string;
+  size?: number;
 };
 
 export const DropDownSelectorLeftIcon: FunctionComponent<DropDownSelectorLeftIconProps> = ({
   icon,
   color,
+  size = 20,
 }) => {
   if (!icon) {
     return null;
   }
 
   return (
-    <Icon source={icon} size={20} color={color} />
+    <Icon source={icon} size={size} color={color} />
   );
 };
