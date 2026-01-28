bc3b7b8f7d4e75ce00226d5362d0d486376e50ae
André Masson <massona@lichens.ai>
2026-01-28 16:21:32 -0400

    feat: list sub-components (#25)

---
+++ src/components/lists/list-header.tsx
@@ -0,0 +1,28 @@
+import { type FunctionComponent } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Text } from 'react-native-paper';
+import { useAppTheme } from '../../theme/theme';
+
+export interface ListHeaderProps {
+  title: string;
+}
+
+export const ListHeader: FunctionComponent<ListHeaderProps> = ({ title }) => {
+  const styles = useStyles();
+
+  return (
+    <View style={styles.container}>
+      <Text variant="titleMedium">{title}</Text>
+    </View>
+  );
+};
+
+const useStyles = () => {
+  const theme = useAppTheme();
+
+  return StyleSheet.create({
+    container: {
+      paddingHorizontal: theme.spacing(1),
+    },
+  });
+};
