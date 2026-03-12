1b42476003a5bb41b1a8d156c2ebdcdc976356b4
André Masson <amwebexpert@gmail.com>
2026-03-12 16:54:58 -0600

    feat: rjsf paper implementation

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import prettierPlugin from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  { ignores: ['src/types/winrt/**', 'src/types/*.d.ts', '**/*.windows.ts', '**/*.windows.tsx'] },
+  { ignores: ['dist/', 'src/types/winrt/**', 'src/types/*.d.ts', '**/*.windows.ts', '**/*.windows.tsx'] },
   {
     files: ['src/**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
